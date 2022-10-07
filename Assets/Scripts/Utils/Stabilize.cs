b37605ab634c0d26c6da518b5f4c63fc9c3acc6b
ScareFire <noe.topeza@epita.fr>
2022-10-07 18:12:53 +0200

    Board stabilised on release

---
+++ Assets/Scripts/Utils/Stabilize.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stabilize : MonoBehaviour
+{
+    private Transform camTransform;
+
+    private void Awake()
+    {
+        camTransform = Camera.main ? Camera.main.transform : null;
+    }
+
+    public void StabilizeBoard()
+    {
+        transform.LookAt(camTransform);
+        var transformRotation = transform.rotation.eulerAngles;
+        transform.rotation = Quaternion.Euler(0, transformRotation.y, transformRotation.z);
+    }
+    
+}
